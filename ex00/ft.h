c00bf2141c120bb88a117ef7a7f97fa7d3acdee6
JanefrancessC <janefrancesschioma@gmail.com>
2024-08-26 19:11:33 +0100

    ex 00

---
+++ ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cokeke <cokeke@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 19:02:55 by cokeke            #+#    #+#             */
+/*   Updated: 2024/08/26 19:10:42 by cokeke           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
